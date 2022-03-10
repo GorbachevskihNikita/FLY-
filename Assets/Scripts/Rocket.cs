389a9ba2d95cb4890cbe4d203cf8a94cab0caeb7
GorbachevskihNikita <nikitagorbachevskih@gmail.com>
2022-03-10 21:36:28 +0500

    create rocket, scene and testscript for rocket

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("A pressed");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("D pressed");
+        }
+    }
+}
