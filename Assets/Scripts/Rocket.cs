df13a0855886480c3bab0014f407603fb65aab8a
GorbachevskihNikita <nikitagorbachevskih@gmail.com>
2022-03-10 22:50:02 +0500

    added rocket control and launch sound

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,36 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+        _rigidbody.mass = 0.7f;
+        _rigidbody.drag = 1.0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        LaunchRocket();
+        RotateRocket();
     }
-
-    void ProcessInput()
+    
+    void LaunchRocket()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Space pressed");
+            _rigidbody.AddRelativeForce(Vector3.up);
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.volume = 0.15f;
+                _audioSource.Play();
+            }
         }
-        
+        else
+        {
+            _audioSource.Pause();
+        }
+    }
+    
+    void RotateRocket()
+    {
+        _rigidbody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A))
         {
-            print("A pressed");
-        }
-
-        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(new Vector3(0,0,0.125f));
+        } 
+        else if (Input.GetKey(KeyCode.D))
         {
-            print("D pressed");
+            transform.Rotate(new Vector3(0, 0 , -0.125f));
         }
+        _rigidbody.freezeRotation = false;
     }
 }
